e8d893597812e41f36fc0411fa37542d8e5212e5
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2024-11-20 16:08:34 -0300

    add tailwind, prettier and eslint config

--- apps/ui/src/App.tsx
+++ apps/ui/src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <p>React + .NET Core</p>;
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold underline">React + .NET Core</h1>
+    </div>
+  );
 }
 
 export default App;
