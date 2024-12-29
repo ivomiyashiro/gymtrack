2441126bc76d504f0233241d697a3fa85bdbd3ca
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2024-12-29 11:53:43 -0300

    chore: remove virtual from entities

--- apps/api/Migrations/20241229144630_Remove_virtual_from_entities.Designer.cs
+++ apps/api/Migrations/20241229144630_Remove_virtual_from_entities.Designer.cs
@@ -4,6 +4,7 @@ using API.AppDbContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241229144630_Remove_virtual_from_entities")]
+    partial class Remove_virtual_from_entities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
